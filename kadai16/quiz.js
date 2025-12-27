eb164b49d8a748d5ba7d0a8e48e3dbce2dd2175f
oooooo <c3240390@std.gku.ac.jp>
2025-12-27 13:31:14 +0900

    ⒥

--- kadai16/quiz.js
+++ kadai16/quiz.js
@@ -3,3 +3,40 @@ function ShowQuiz(){
     document.getElementById("quizPage").classList.remove("NewPage");
 
 }
+
+function showResult(isCorrect) {
+    const result = document.getElementById("result");
+    const nextBtn = document.getElementById("nextPage");
+
+    if (isCorrect) {
+        result.textContent = "〇 正解！";
+    } else {
+        result.textContent = "✕ 不正解　正解：４番 Atlantis";
+    }
+
+    nextBtn.style.display = "block";
+}
+
+
+function A1() {
+    showResult(false);
+}
+
+function A2() {
+    showResult(false);
+}
+
+function A3() {
+    showResult(false);
+}
+
+
+function A4() {
+    showResult(true);
+}
+
+function nextQuestion() {
+
+    document.getElementById("result").textContent = "";
+    document.getElementById("nextPage")
+}
